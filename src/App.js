c4fd2355c3b90ba8c21a8a32650aab306378fc53
bhuvan malik <bhuvanmalik007@gmail.com>
2020-08-13 09:11:39 -0400

    Initialized with Material-UI, added Header & light/dark mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import Header from "./components/Header";
+import { CssBaseline } from "@material-ui/core";
+
+
+const useStyles = makeStyles((_theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    width: "100%",
+  },
+  button: {
+    color: "#00a152",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [darkMode, setDarkMode] = useState(false);
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: darkMode ? "dark" : "light",
+        },
+        typography: {
+          fontFamily: [
+            "Noto Sans JP",
+            "-apple-system",
+            "BlinkMacSystemFont",
+            '"Segoe UI"',
+            "Roboto",
+            '"Helvetica Neue"',
+            "Arial",
+            "sans-serif",
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+          ].join(","),
+        },
+        overrides: {
+          MuiSlider: {
+            thumb: {
+              color: "black",
+            },
+            track: {
+              color: "grey",
+            },
+          },
+        },
+      }),
+    [darkMode]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className={classes.root}>
+        <Header darkMode={darkMode} setDarkMode={setDarkMode} title="Order Menu" />
+      </div>
+    </ThemeProvider>
   );
 }
 
